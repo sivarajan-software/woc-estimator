3e490cb32efb9e70107c3b386792b571fdf314ea
Sivarajan Vedhachalam <sivarajan.software@gmail.com>
2021-08-17 17:10:56 +0530

    test

--- pod.js
+++ pod.js
@@ -25,7 +25,8 @@ function getEstimate() {
     document.getElementById("result").innerText = "Total Cost: Rs. " + data.jobCost + "\nEach Book: Rs. " + data.eachBook;
     console.log(output);
   }
-  xhttp.open("POST", "http://localhost:5001/vstpress-site2/us-central1/getWOCEstimate");
+  // xhttp.open("POST", "http://localhost:5001/vstpress-site2/us-central1/getWOCEstimate");
+  xhttp.open("POST", "https://us-central1-vstpress-site2.cloudfunctions.net/getWOCEstimate");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify(input));
 
