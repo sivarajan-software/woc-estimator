3a2862f4fa982fefc7dc07d8612fd01862e43655
Sivarajan Vedhachalam <sivarajan.software@gmail.com>
2021-08-20 11:12:33 +0530

    calendar estimate

--- pod.js
+++ pod.js
@@ -1,4 +1,4 @@
-function getEstimate() {
+function getBookEstimate() {
   const bookSize = document.getElementById("bookSize").value;
   const innerPages = document.getElementById("innerPages").value;
   const innerPaper = document.getElementById("innerPaper").value;
@@ -8,6 +8,7 @@ function getEstimate() {
   const quantity = document.getElementById("quantity").value;
 
   input = {
+    "type": "book",
     "bookSize": bookSize,
     "innerPages": parseInt(innerPages),
     "innerPaper": innerPaper,
@@ -17,19 +18,62 @@ function getEstimate() {
     "quantity": parseInt(quantity),
   };
 
-  output = {};
+  getEstimate(input, (result) => {
+    console.log(result);
+    const totalStr = `Total Cost: ${toTotalCurrency(result.total)}/-`;
+    const rateStr = `Each Book: ${toRateCurrency(result.rate)}`;
+    document.getElementById("result").innerText = totalStr + "\n" + rateStr;
+  });
 
+  return false;
+}
+
+function toTotalCurrency(number) {
+  return number.toLocaleString('en-IN', { maximumFractionDigits: 0, style: "currency", currency: "INR" });
+}
+
+function toRateCurrency(number) {
+  return number.toLocaleString('en-IN', { maximumFractionDigits: 2, minimumFractionDigits: 2, style: "currency", currency: "INR" });
+}
+
+function getCalendarEstimate() {
+  const calendarSize = document.getElementById("calendarSize").value;
+  const sheets = document.getElementById("sheets").value;
+  const paper = document.getElementById("paper").value;
+  const printSides = document.getElementById("printSides").value;
+  const binding = document.getElementById("binding").value;
+  const quantity = document.getElementById("quantity").value;
+
+  input = {
+    "type": "calendar",
+    "calendarSize": calendarSize,
+    "sheets": parseInt(sheets),
+    "paper": paper,
+    "printSides": printSides,
+    "binding": binding,
+    "quantity": parseInt(quantity),
+  };
+
+  getEstimate(input, (result) => {
+    console.log(result);
+    const totalStr = `Total Cost: ${toTotalCurrency(result.total)}/-`;
+    const rateStr = `Each Rate: ${toRateCurrency(result.rate)}`;
+    document.getElementById("result").innerText = totalStr + "\n" + rateStr;
+  });
+
+  return false
+}
+
+function getEstimate(input, callback) {
   const xhttp = new XMLHttpRequest();
   xhttp.onload = function () {
     var data = JSON.parse(this.responseText);
-    document.getElementById("result").innerText = "Total Cost: Rs. " + data.jobCost + "\nEach Book: Rs. " + data.eachBook;
-    console.log(output);
+    callback(data);
   }
-  // xhttp.open("POST", "http://localhost:5001/vstpress-site2/us-central1/getWOCEstimate");
-  xhttp.open("POST", "https://us-central1-vstpress-site2.cloudfunctions.net/getWOCEstimate");
+  xhttp.open("POST", "http://localhost:5001/vstpress-site2/us-central1/getWOCEstimate");
+  // xhttp.open("POST", "https://us-central1-vstpress-site2.cloudfunctions.net/getWOCEstimate");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify(input));
-  return false;
 }
 
 function clearOutput() {
